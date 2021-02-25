291c584682f794eec266e6fc34c90233541188f0
thrwat101 <31253314+thrwat101@users.noreply.github.com>
2021-02-25 10:26:18 -0500

    Add files via upload

---
+++ PidManager.java
@@ -0,0 +1,25 @@
+public class PidManager {
+
+    public static void allocate_map(){
+        
+    }
+    
+    public static int allocate_pid(){
+
+    }
+
+    public static int release_pid(int pid){
+
+    }
+
+    private int MIN_PID = 300;
+    private int MAX_PID = 5000;
+    private int[] pids;
+
+    public static void main(String[] args){
+
+
+        
+    }
+    
+}
