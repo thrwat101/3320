14964766079ad6c1fe096183dc210be0c22c2bf4
Thrwat Najjar <thrwat101@gmail.com>
2021-02-25 13:52:33 -0500

    update

--- PidManager.java
+++ PidManager.java
@@ -1,25 +1,52 @@
+import java.util.*;
+import java.io.*;
+
 public class PidManager {
 
-    public static void allocate_map(){
-        
+  public static int allocate_map(){
+    pids = new HashMap();
+     
+    if(pids == null)
+      return -1;
+     
+    for(int i = 0; i < MAX_PID+1; i++)
+      pids.put(i, 0);
+  
+    return 1;
+  }
+  
+public static int allocate_pid(){
+  if(pids == null) 
+      return -1;
+
+    int pi = 0;
+
+    for(int i = MIN_PID; i <= MAX_PID; i++){
+      if(pids.get(i).equals(0)) {
+        pi = i;
+        pids.put(pi, 1);
+        return pi;
+      }
     }
     
-    public static int allocate_pid(){
+    return 1;
+ }
 
-    }
+  //public int release_pid(int pid){
 
-    public static int release_pid(int pid){
+ // }
 
-    }
+  private static int MIN_PID = 300;
+  private static int MAX_PID = 5000;
+  private static Map pids;
+  
+  public static void main(String[] args){
 
-    private int MIN_PID = 300;
-    private int MAX_PID = 5000;
-    private int[] pids;
-
-    public static void main(String[] args){
-
-
-        
-    }
+      allocate_map();
+      System.out.println(allocate_pid());
+      System.out.println(allocate_pid());
+      System.out.println(allocate_pid());
+      
+  }
     
 }
