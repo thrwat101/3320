7612ea959fcf0dd5545000ca5d7eb5eb01b6f2f2
Thrwat Najjar <thrwat101@gmail.com>
2021-02-26 00:19:41 -0500

    Update PidManager.java

--- PidManager.java
+++ PidManager.java
@@ -4,16 +4,16 @@ import java.io.*;
 public class PidManager {
 
   public static int allocate_map(){
-    pids = new HashMap();
+    pids = new HashMap();//hashmap to store pids and there values.
 
-    for(int i = 0; i < MAX_PID+1; i++)
+    for(int i = 0; i < MAX_PID+1; i++)//sets all the values of the keys to zero.
       pids.put(i, 0);
       
     return 1;
   }
   
   public static int allocate_pid(){
-    if(pids.isEmpty())
+    if(pids.isEmpty())//checks if PidManager has not been initialized.
       return -1;
 
     int pid = 0;
@@ -35,8 +35,13 @@ public class PidManager {
 
   public static void release_pid(int pid){
     if(pids.isEmpty())
-      return -1;
+      System.out.println("Pid Manager has not been initiated.");
     
+    if(pid > MAX_PID || pid < MIN_PID)
+      System.out.println("This pid is out of range.");
+    
+    pids.put(pid, 0);
+    System.out.println("released pid: " + pid);
     
   }
 
@@ -48,10 +53,11 @@ public class PidManager {
     allocate_map();
     System.out.println(allocate_map());
 
-    for(int i = 0; i <= 4701; i++){
+    for(int i = 0; i <= 5; i++){
       System.out.println("Allocated pid: " + allocate_pid());
 
     }
+    release_pid(302);
     
   }
     
