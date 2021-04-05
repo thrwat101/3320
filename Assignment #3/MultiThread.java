61690905350e5d4157f593f87d58ce841751c881
Thrwat Najjar <thrwat101@gmail.com>
2021-04-05 16:15:52 -0400

    Hw3 folder creation

---
+++ Assignment #3/MultiThread.java
@@ -0,0 +1,80 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static java.lang.Thread.currentThread;
+public class MultiThread implements Runnable{
+    public static int thread_id = 0;
+    private int sleep_time;
+    private PidManager pids = new PidManager();
+
+    MultiThread(int thread_id, int sleep_time, PidManager pids) {
+        this.thread_id = thread_id;
+        this.sleep_time = sleep_time;
+        this.pids = pids;
+        System.out.println("Creating Thread: " + thread_id);
+    }
+
+    MultiThread(int thread_id) {
+        this.thread_id = thread_id;
+        sleep_time = (int) (Math.random() * 1000);
+        pids.allocate_map();
+        System.out.println("Creating Thread: " + thread_id); 
+    }
+
+    MultiThread() {
+        thread_id++;
+        sleep_time = (int) (Math.random() * 1000);
+        pids.allocate_map();
+        System.out.println("Creating Thread: " + thread_id); 
+    }
+
+    @Override
+    public void run() {
+        Integer pid;
+
+        System.out.println("Running Thread: " + currentThread().getName()); 
+        pid = pids.allocate_pid();                                    
+
+        while (pid == -1) {                                         
+            System.out.println("All PIDs are in use");
+            pid = pids.allocate_pid();
+        }
+
+
+        currentThread().setName(pid.toString());                     
+        System.out.println("Allocated PID: " + pid);
+
+
+        try {
+            Thread.sleep(sleep_time);                                 
+
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + currentThread().getName() + " interrupted.");
+        }
+
+        Integer release_pid = Integer.valueOf(currentThread().getName()); 
+
+        pids.release_pid(release_pid);                                   
+
+        System.out.println("Exiting Thread: " + currentThread().getName()); 
+    }
+
+    public static class ThreadPoolTest {
+        public static void main(String args[]) {
+            PidManager pm = new PidManager();
+            pm.allocate_map();
+
+            //creating thread pools for each multi thread
+            ExecutorService pool = Executors.newFixedThreadPool(5); 
+
+            for (int i = 1; i <= 5; i++) {
+                MultiThread task1 = new MultiThread(i, (int) (Math.random() * 1000), pm);  
+                pool.execute(task1);    
+            }
+
+            pool.shutdown();
+  
+        }
+
+    }
+
+}
---
+++ Assignment #3/PidManager.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+public class PidManager {
+
+  public static int allocate_map(){
+    pids = new HashMap();//hashmap to store pids and there values.
+
+    try{
+      for(int i = 0; i < MAX_PID+1; i++)//sets all the values of the keys to zero.
+        pids.put(i, 0);
+
+    }catch(Exception e){
+      return -1;
+    }  
+    return 1;
+    
+  }
+  
+  public static int allocate_pid(){
+    int pid = 0;
+    boolean isFull = true;
+
+    if(pids == null)//checks if PidManager has not been initialized.
+      return -1;
+      
+    for(int i = MIN_PID; i <= MAX_PID; i++){//only allocates pids within the min and max range.
+      if(pids.get(i).equals(0)) {
+        isFull = false;
+        pid = i;
+        pids.put(pid, 1);
+        return pid;
+      } 
+    }
+
+    if(isFull) //checks if all the pids are used.
+        return -1;
+    else 
+      return pid;
+  }
+ 
+  public static void release_pid(int pid){
+    if(pids == null)
+      System.out.println("Pid Manager has not been initialized.");
+    
+    else if(pid > MAX_PID || pid < MIN_PID) //make sure pid is within the min and max range.
+      System.out.println("This pid is out of range.");
+    
+    else {
+      pids.put(pid, 0);//resets the value to zero. so pid is no longer in use.
+      System.out.println("released pid: " + pid);
+    }
+    
+  }
+
+  private static int MIN_PID = 300;
+  private static int MAX_PID = 5000;
+  private static Map<Integer, Integer> pids;
+  
+  public static void main(String[] args){
+    PidManager pid1 = new PidManager();
+    pid1.allocate_map();
+
+    for(int i = 0; i <= 5; i++){
+      System.out.println("Allocated pid: " + pid1.allocate_pid());
+
+    }
+    
+    pid1.release_pid(302);
+    
+  }
+    
+}
